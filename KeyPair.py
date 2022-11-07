79d266f7e0875a77cdeefc31fbec76c1116dbf96
Noel Mbuya <mbuyanoel@gmail.com>
2022-11-07 07:14:03 +0300

    Add KeyPair and Signature class implementations

--- KeyPair.py
+++ KeyPair.py
@@ -1,1 +1,50 @@
-# A class for working with keys.
+from ecdsa import SigningKey, VerifyingKey, SECP256k1
+
+
+class KeyPair:
+        
+    def __init__(self, privateKey, publicKey):
+        self.privateKey = privateKey
+        self.publicKey = publicKey
+            
+    # privateKey, publicKey = '', ''
+    global privateKey, publicKey 
+
+
+    def genKeyPair():            
+        # Generating the key pair from a SECP256K1 elliptic curve.
+        global privateKey, publicKey
+        privateKey = SigningKey.generate(curve=SECP256k1)
+        publicKey = privateKey.get_verifying_key()
+        
+        with open("priv_key.pem", "wb") as f:
+            f.write(privateKey.to_pem(format="pkcs8"))
+        with open("pub_key.pem", "wb") as f:
+            f.write(publicKey.to_pem())   
+        return privateKey, publicKey 
+
+
+    def printKeyPair():    
+        # print(privateKey)
+        # print(privateKey, publicKey)
+        with open("priv_key.pem") as f:
+            pri_key_string = SigningKey.from_pem(f.read())
+            print(pri_key_string)
+        with open("pub_key.pem") as f:
+            pub_key_string = VerifyingKey.from_pem(f.read())
+            print(pub_key_string)
+            
+    def toString():
+        with open("priv_key.pem") as f:
+            pri_key_string = SigningKey.from_pem(f.read())
+            print(pri_key_string)
+        with open("pub_key.pem") as f:
+            pub_key_string = VerifyingKey.from_pem(f.read())
+            print(pub_key_string)
+        return pri_key_string, pub_key_string
+
+    # Calling functions to test their working
+    printKeyPair()
+
+
+
--- Signature.py
+++ Signature.py
@@ -1,1 +1,46 @@
-# A wrapper class for using a digital signature (any algorithm).
+from KeyPair import KeyPair, privateKey, publicKey
+import hashlib 
+from ecdsa.util import sigencode_der
+from ecdsa.util import sigdecode_der
+from ecdsa import BadSignatureError
+# from pathlib import Path
+
+
+# privateKey = KeyPair.genKeyPair()
+privateKey = KeyPair.privateKey       # Having trouble importing this two properties from KeyPair class. Still working on them
+publicKey = KeyPair.publicKey          
+hash_func = hashlib.sha256()
+data = "Some test data"
+
+def signData(self, data, privateKey):
+    self.data = data
+    self.privateKey = privateKey
+    sig = privateKey.sign_deterministic(
+        data,
+        hash_func,
+        sigencode=sigencode_der
+        ) 
+        
+    with open("data.sig", "wb") as f:
+        f.write(sig)
+    return 
+
+def verifySignature(self, data):
+    self.data = data
+    with open("data.sig", "rb") as f:
+        sig = f.read()
+        
+    try:
+        ret = publicKey.verify(sig, data, hash_func, sigdecode=sigdecode_der)
+        assert ret
+        print("Valid signature")
+    except BadSignatureError:
+        print("Incorrect signature")
+        
+def toString():
+    pass
+
+
+# signData(data, priv_key.pem)
+# verifySignature(dat)
+signData()
